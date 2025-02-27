16775cdf3235eb9fc1606f53ea07e5134305a3fa
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-02-26 18:58:34 -0600

    finished js and html

--- index.js
+++ index.js
@@ -16,3 +16,86 @@ const zero = document.getElementById("zero");
 const dot = document.getElementById("dot");
 const equals = document.getElementById("equals");
 const plus = document.getElementById("plus");
+
+
+c.addEventListener("click", () => {
+    screen.value ="";
+})
+
+one.addEventListener("click", () => {
+    screen.value += 1;
+})
+
+two.addEventListener("click", () => {
+    screen.value += 2;
+})
+
+three.addEventListener("click", () => {
+    screen.value += 3;
+})
+
+divide.addEventListener("click", () => {
+    screen.value += "/";
+})
+
+four.addEventListener("click", () => {
+    screen.value += 4;
+})
+
+five.addEventListener("click", () => {
+    screen.value += 5;
+})
+
+six.addEventListener("click", () => {
+    screen.value += 6;
+})
+
+multiply.addEventListener("click", () => {
+    screen.value += "*";
+})
+
+seven.addEventListener("click", () => {
+    screen.value += 7;
+})
+
+eight.addEventListener("click", () => {
+    screen.value += 9;
+})
+
+nine.addEventListener("click", () => {
+    screen.value += 9;
+})
+
+minus.addEventListener("click", () => {
+    screen.value += "-";
+})
+
+zero.addEventListener("click", () => {
+    screen.value += 0;
+})
+
+dot.addEventListener("click", () => {
+    screen.value += ".";
+})
+
+plus.addEventListener("click", () => {
+    screen.value += "+";
+})
+
+equals.addEventListener("click", () => {
+    let x = eval(screen.value);
+    screen.value = x;
+})
+
+window.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+
+        let x = eval(screen.value);
+        screen.value = x;
+
+    }
+})
+
+
+
